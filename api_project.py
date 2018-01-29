56a1e5fb66acb48ed38034de26804f644ea2cc58
Shivani Bhatia <scbhatia@bu.edu>
2018-01-29 18:24:38 -0500

    finished twitter

--- api_project.py
+++ api_project.py
@@ -8,6 +8,11 @@ import sys
 import os
 from tweepy import API, OAuthHandler, Stream
 
+import json
+
+import io
+import google.cloud.vision
+
 def config_parse(filename):
     cparse = configparser.ConfigParser()
     cparse.read(filename)
@@ -19,12 +24,11 @@ def authorization(cparse):
     api = API(auth)
     return api
 
+#Retrieves tweets from given twitter handle and downloads images to a specified folder 
 def downloadTweets(username, tweet_count, api, output):
     try:
         tweets = api.user_timeline(screen_name=username, count=300, include_rts=False, exclude_replies=True)
-        saved = 0
-        
-        
+
     except Exception as e:
         print(e)
         sys.exit()
@@ -41,8 +45,41 @@ def downloadTweets(username, tweet_count, api, output):
     except:
         os.chdir(username)
 
-    if not 
+    saved = 0
+    length = len(tweets)
+    media_tweets = set()
     
+    while (length > 0):
+        last_post = tweets[-1].id - 1
+
+        for post in tweets:
+            if hasattr(post, 'extended_entities'):
+                images = post.extended_entities.get('media',[])
+                image_len = len(images)
+
+                if (image_len != 0):
+                    if 'media' in post.extended_entities:
+                        for media_files in tweet.extended_entities['media']:
+                            if not 'video_info' in media_files:
+                                for image_media in tweet.extended_entities['media']:
+                                    media_tweets.add(image_media['media_url'])
+
+    for links in media_tweets:
+        wget.download(links, out="output")
+                                    
+
+#ffmpeg compiles all of the images into a video file
+def makeVideo():
+
+
+#Google Cloud vision does analysis on each image in the specified folder 
+def doAnalysis(output):
+    vision_client = google.cloud.vision.ImageAnnotatorClient()
+
+    for files in output:
+        print(files)
+        
+
     
 def main():
     cparse = config_parse("config.cfg")
@@ -54,7 +91,8 @@ def main():
     
     username = input("\nPlease enter a twitter handle: ")
     output = input("\What is the name of the folder you would like the files to be stored in? ")
-    tweet_count = 50;
+    tweet_count = input("\How many images would you like in your video?")
 
-    posts = getTweets(username, tweet_count, api, output)
+    posts = downloadTweets(username, tweet_count, api, output)
+    analysis = doAnalysis(output)
     
